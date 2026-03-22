f5f6434c9141c7ddf9535e57f7bdcc852288c0f8
Luís Fernando <contactluisfernandosg@gmail.com>
2026-03-22 17:09:06 -0300

    Adicionado função 'listarProdutosVendidos()

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -95,6 +95,38 @@ public class ProdutosDAO {
             e.printStackTrace();
             return false;
         }
-    }  
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+        String sql = "SELECT * FROM produtos WHERE status = ?";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, "Vendido");
+
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listagem;
+    }
 }
 
